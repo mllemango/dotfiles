0112533dcf234aadc8322cbf2ce2fedd7d99e9cb
Melanie Wang <melanie.wang@shopify.com>
2021-08-03 11:00:35 -0400

    install spaceship

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zshrc ~/.zshrc
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install spaceship theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Reload zsh config
+source ~/.zshrc
