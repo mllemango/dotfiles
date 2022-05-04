1e5a0607ddb7f66fa12abf605fd8ab7bd0d56df3
Melanie Wang <melanie.wang@shopify.com>
2022-05-04 11:22:29 -0400

    Conditional spaceship

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
 fi
 
 ZSH_CUSTOM="/home/spin/.oh-my-zsh/custom"
-echo "\n -- SPACESHIP TIME -- \n"
+
 # Install spaceship theme
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+if [[ ! -d $ZSH_CUSTOM/themes/spaceship-prompt ]]; then
+  echo "\n -- SPACESHIP TIME -- \n"
+
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
 # install ohmyzsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
